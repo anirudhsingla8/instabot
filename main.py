3dd530865971332f88618bfba1ee1a476d88d1c5
anirudhsingla8 <anirudhsingla8@gmail.com>
2017-07-05 19:29:47 +0530

    new change

--- main.py
+++ main.py
@@ -81,6 +81,28 @@ def get_own_post():
         print 'Status code other than 200 received!'
 
 
+# to get user post
+def get_user_post(insta_username):
+    user_id = get_user_id(insta_username)
+    if user_id == None:
+        print 'User does not exist!'
+        exit()
+    request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_media = requests.get(request_url).json()
+
+    if user_media['meta']['code'] == 200:
+        if len(user_media['data']):
+            image_name = user_media['data'][0]['id'] + '.jpeg'
+            image_url = user_media['data'][0]['images']['standard_resolution']['url']
+            urllib.urlretrieve(image_url, image_name)
+            print 'Your image has been downloaded!'
+        else:
+            print 'Post does not exist!'
+    else:
+        print 'Status code other than 200 received!'
+
+
 def start_bot():
     while True:
         print '\n'
